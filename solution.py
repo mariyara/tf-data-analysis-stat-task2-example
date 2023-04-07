6d64697b3ed145f5b38a5e3ccefc75ba1043b3c3
mariyara <70849273+mariyara@users.noreply.github.com>
2023-04-07 22:53:44 +0300

    gogoasymptoticinterval

--- solution.py
+++ solution.py
@@ -4,14 +4,13 @@ import numpy as np
 from scipy.stats import norm
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 558620654 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
     alpha = 1 - p
-    loc = x.mean()
-    scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
-    return loc - scale * norm.ppf(1 - alpha / 2), \
-           loc - scale * norm.ppf(alpha / 2)
+    S = np.sqrt(1/(len(x)-1)*np.sum((x-x.mean())**2))
+    z2 = norm.ppf(1-alpha/2)
+    z1 = norm.ppf(alpha/2)
+    left = 2*x.mean()+2*z1*S/np.sqrt(len(x))
+    right = 2*x.mean()+2*z2*S/np.sqrt(len(x))
+    return left, right
