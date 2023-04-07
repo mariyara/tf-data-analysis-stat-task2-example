56159e8c14f9bf6a03a7a6a6d2de30eb8b7b561c
mariyara <70849273+mariyara@users.noreply.github.com>
2023-04-08 01:10:49 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,15 +2,22 @@ import pandas as pd
 import numpy as np
 
 from scipy.stats import norm
+from scipy.stats import expon
 
 
 chat_id = 558620654 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    alpha = 1 - p
-    S = np.sqrt(1/(len(x)-1)*np.sum((x-x.mean())**2))
-    z2 = norm.ppf(1-alpha/2)
-    z1 = norm.ppf(alpha/2)
-    left = 2*x.mean()+2*z1*S/np.sqrt(len(x))
-    right = 2*x.mean()+2*z2*S/np.sqrt(len(x))
+    # Измените код этой функции
+    # Это будет вашим решением
+    # Не меняйте название функции и её аргументы
+    alpha=1-p
+    y = x-0.032
+    #e1+e2=1-p
+    e1=alpha/2
+    e2=1-alpha/2
+    n=len(y)
+    left = np.max(y)/(e2)**(1/n)+0.032
+    right = np.max(y)/(e1)**(1/n)+0.032
+
     return left, right
